8ec3d7f6b0448d53304c6763614371575e2f7e5b
hami-r <mynameissaitama54@gmail.com>
2022-11-25 12:35:55 +0530

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { ViewTodoComponent } from './view-todo/view-todo.component';
+import { NavbarComponent } from './view-todo/navbar.component';
 
 @NgModule({
   declarations: [
